75587ce56329a40f14cbd003165354cf34590b1d
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-26 09:50:17 +0200

    pages showing on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import './App.scss'
-import './pages/PageWelcome';
-import './pages/PageEmployees';
-import './pages/PageTechBooks';
+import './App.scss';
+import { PageWelcome } from './pages/PageWelcome';
+import { PageEmployees } from './pages/PageEmployees';
+import { PageTechBooks } from './pages/PageTechBooks';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>React Router Site</h1>
-    
-      
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>React Router Site</h1>
+
+            <PageWelcome />
+            <PageEmployees/>
+            <PageTechBooks/>
+        </div>
+    );
 }
 
-export default App
+export default App;
