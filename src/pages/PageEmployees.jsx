07779a2a155108a0b9a67b1207e5b3f41cec7b37
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-26 10:59:59 +0200

    lists employees with dummy data

--- src/pages/PageEmployees.jsx
+++ src/pages/PageEmployees.jsx
@@ -1,7 +1,17 @@
-export const PageEmployees = ({employees}) => {
+export const PageEmployees = ({ employees }) => {
 	return (
 		<>
 			<p>There are the {employees.length} employees:</p>
+
+			<div className="employees">
+				{employees.map((emp, i) => {
+					return (
+						<div className="employee" key={i}>
+							<div className="fullName">full name</div>
+						</div>
+					);
+				})}
+			</div>
 		</>
 	);
 };
